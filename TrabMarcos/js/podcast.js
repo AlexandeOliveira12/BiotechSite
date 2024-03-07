7794dc450ed12ace7ae05c79e94992dfd5cb9984
Alexandre Oliveira <105618045+AlexandeOliveira12@users.noreply.github.com>
2024-03-06 23:58:46 -0300

    Delete TrabMarcos directory

--- TrabMarcos/js/podcast.js
+++
@@ -1,8 +0,0 @@
-let doctitle = document.title;
-window.addEventListener("blur", () =>{
-    document.title = "Volte aqui 😢";
-})
-
-window.addEventListener("focus", () =>{
-    document.title = doctitle;
-})
